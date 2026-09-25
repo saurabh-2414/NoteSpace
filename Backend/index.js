ce4c476ae68f2f28e93c1b67fb1a84ab79cf592e
Saurabh Prajapati <saurabhprajapati651@gmail.com>
2026-09-25 23:34:52 +0530

    Allow localhost and production frontend CORS

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,6 @@
-require("dotenv").config({ path: "./backend/.env" });
+// require("dotenv").config({ path: "./backend/.env" });
+
+require("dotenv").config();
 
 const connectToMongo = require("./db");
 const express = require("express");
@@ -10,11 +12,31 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+
+//fro both local host and deployment
+const allowedOrigins = [
+  "http://localhost:3000",
+  process.env.FRONTEND_URL,
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   }),
 );
+
+//for only deployement
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:3000",
+//   }),
+// );
 
 app.use(express.json());
 
